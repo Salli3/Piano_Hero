c7deacdb9080e1b9b97034f037ce604754e9ff95
Salli3 <dpha553.w@gmail.com>
2026-07-16 19:28:55 +0700

    feat: judgement box hit respond

--- Assets/Scripts/Note_Scripts/Judgement_Box.cs
+++ Assets/Scripts/Note_Scripts/Judgement_Box.cs
@@ -5,20 +5,39 @@ using UnityEngine;
 public class Judgement_Box : MonoBehaviour
 {
     [SerializeField] private Collider2D judgeBoxCollider;
+    [SerializeField] private SpriteRenderer sr;
     [SerializeField] private LayerMask noteLayer;
     public KeyCode triggerKey;
 
     private void Awake()
     {
         judgeBoxCollider = GetComponent<BoxCollider2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     public void GetOverLappingNote()
     {
+        StartCoroutine(hitRespond());
+
         Collider2D[] hits = Physics2D.OverlapBoxAll(judgeBoxCollider.bounds.center, judgeBoxCollider.bounds.size, 0, noteLayer);
         if (hits.Length > 0)
         {
             hits[0].GetComponent<Note>().OnNoteHit();
         }
+        else
+        {
+            sr.color = Color.red;
+        }
+    }
+
+    private IEnumerator hitRespond()
+    {
+        Vector3 originalScale = transform.localScale;
+        transform.localScale = originalScale * 1.2f;
+
+        yield return new WaitForSeconds(0.1f);
+
+        transform.localScale = originalScale;
+        sr.color = Color.white;
     }
 }
