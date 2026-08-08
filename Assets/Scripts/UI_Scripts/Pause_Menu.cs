b1ca347273c1ab57dd6df00b5470f8aa77fae122
Salli3 <dpha553.w@gmail.com>
2026-08-08 16:50:25 +0700

    feat: pause menu

---
+++ Assets/Scripts/UI_Scripts/Pause_Menu.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause_Menu : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup pauseCanvasGroup;
+    private bool isPaused = false;
+
+    private void Start()
+    {
+        pauseCanvasGroup.alpha = 0;
+        pauseCanvasGroup.interactable = false;
+        pauseCanvasGroup.blocksRaycasts = false;
+        Time.timeScale = 1;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isPaused)
+            {
+                Debug.Log("Escape key pressed!");
+                Time.timeScale = 0;
+                pauseCanvasGroup.alpha = 1;
+                pauseCanvasGroup.interactable = true;
+                pauseCanvasGroup.blocksRaycasts = true;
+                isPaused = true;
+            }
+            else
+            {
+                Resume();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        pauseCanvasGroup.alpha = 0;
+        pauseCanvasGroup.interactable = false;
+        pauseCanvasGroup.blocksRaycasts = false;
+        isPaused = false;
+    }
+
+    public void Quit()
+    {
+        Game_Manager.instance.CleanUpAndDestroy();
+        SceneManager.LoadScene("Main_Menu");
+    }
+}
